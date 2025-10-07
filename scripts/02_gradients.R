ffc5d53b54d9b4dd73bf020a94d5f266cc3830b7
mcorinahl <mchl0709@gmail.com>
2025-10-06 23:31:04 -0500

    Update 02_gradients.R

--- scripts/02_gradients.R
+++ scripts/02_gradients.R
@@ -226,8 +226,8 @@ props <- props %>%
   id_prop = dplyr::row_number()) %>%
   filter(!is.na(operation)) %>%
   dplyr::select(
-    id_prop, operation, price_m2_venta, rent_m2_mo,
-    dist_ci_km, ln_st, ln_sc,
+    id_prop, operation, price, price_m2_venta, rent_m2_mo,
+    dist_ci_km, ln_st, ln_sc, surface_total, 
     bedrooms, bathrooms, rooms,
     cod_upz, geom
   )
